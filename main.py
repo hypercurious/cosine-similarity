5ce43acc8dab6af6aaa55349065332d9778188d2
Antonis Pappas <48538391+papagrammer@users.noreply.github.com>
2019-06-17 08:22:30 +0300

    Minor additions
    Almost done

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ discrete_words = []
 documents_name = []
 documents_dictionary = {}
 common_words = {}
-words_counter = 0
-number_of_documents = 0
+counter_list = []
 
 def cosine_similarity(document1, document2):
   norm_doc1 = np.linalg.norm(document1)
@@ -16,14 +15,34 @@ def cosine_similarity(document1, document2):
   cos = dot_product / vector_length_product
   return cos
 
-number_of_documents = int(input("Enter the number of documents: "))
+x = 0
+while x!=1:
+  try:
+    number_of_documents = int(input("Enter the number of documents: "))
+    x = 1
+  except ValueError:
+    print("Invalid option, please type an integer!")
+
 for i in range(number_of_documents):
-  documents_name.append(input("Enter the name (including file type) of document #" + str(i+1) + ": "))
-  current_document = open(documents_name[i], "r")
-  document_content = current_document.read().lower().split()
-  for word in document_content:
-    if word not in discrete_words:
-      discrete_words.append(word)
-  documents_dictionary.update({documents_name[i]: document_content})
-  current_document.close()
+  x = 0
+  while x!=1:
+    try:
+      temp = input("Enter the name (including file type) of document #" + str(i+1) + ": ")
+      current_document = open(temp, "r")
+      documents_name.append(temp)
+      document_content = current_document.read().lower().split()
+      for word in document_content:
+        if word not in discrete_words:
+          discrete_words.append(word)
+      documents_dictionary.update({documents_name[i]: document_content})
+      current_document.close()
+      x = 1
+    except FileNotFoundError:
+      print("Invalid option, file not found, try again!")
+
+for document in documents_dictionary:
+  for word in discrete_words:
+    counter_list.append(documents_dictionary[document].count(word))
+  common_words.update({document: counter_list})
+  counter_list = []
 
